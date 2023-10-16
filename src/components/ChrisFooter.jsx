ce62090c4edbc81d47a20a76f7d560f3cf6eaa70
anthonylonsdale <alons3253@gmail.com>
2023-10-15 22:38:00 -0500

    form and footer changes

--- src/components/ChrisFooter.jsx
+++ src/components/ChrisFooter.jsx
@@ -1,6 +1,6 @@
-import { Divider, Image, Layout, Typography } from 'antd';
+import { Image, Layout, Typography } from 'antd';
 import React from 'react';
-import './Footer.css';
+import styles from './Footer.module.css';
 
 import logo from '../images/clonsdale_cmpnphoto.jpeg';
 
@@ -9,22 +9,27 @@ const { Title } = Typography
 
 function AppFooter() {
   return (
-    <Footer className="footer">
-      <Divider />
-      <Image
-        src={logo}
-        alt="Logo"
-        style={{width: '20rem'}}
-        preview={false}
+    <Footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <Image
+          src={logo}
+          alt="Logo"
+          style={{ width: '20rem' }}
+          preview={false}
         />
-      <Title level={5}>
-        Paid for by Citizens for Chris Lonsdale
-      </Title>
-      <Title level={5}>
-        Howard Lonsdale, Treasurer
-      </Title>
+        <div>
+          <Title level={5} className={styles.footerText}>
+            Paid for by Citizens for Chris Lonsdale
+          </Title>
+        </div>
+        <div>
+          <Title level={5} className={styles.footerText}>
+            Howard Lonsdale, Treasurer
+          </Title>
+        </div>
+      </div>
     </Footer>
-  )
+  );
 }
 
 export default AppFooter
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,7 @@
+import { FacebookOutlined } from '@ant-design/icons';
+import { Button, Form, Input, message } from 'antd';
 import React, { useState } from 'react';
 import styles from './ContactForm.module.css';
-
-import { message } from 'antd';
 
 function ContactForm() {
     const [formData, setFormData] = useState({
@@ -19,36 +19,46 @@ function ContactForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit} className={styles.form}>
-            <div className={styles.inputGroup}>
-                <input
+        <div className={styles.form}>
+            <div className={styles.flexContainer}>
+                <a href="https://www.facebook.com/your-facebook-page-url">
+                    <FacebookOutlined style={{ fontSize: '2rem'}} />
+                </a>
+            </div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Item>
+                    <Input
                     type="text"
                     placeholder="Name"
                     value={formData.name}
                     onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                    className={styles.input}
-                />
-            </div>
-            <div className={styles.inputGroup}>
-                <input
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Input
                     type="email"
                     placeholder="Email"
                     value={formData.email}
                     onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                    className={styles.input}
-                />
-            </div>
-            <div className={styles.inputGroup}>
-                <textarea
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Input.TextArea
                     placeholder="Message"
                     value={formData.message}
                     onChange={(e) => setFormData({ ...formData, message: e.target.value })}
-                    className={styles.textarea}
-                ></textarea>
-            </div>
-            <button type="submit" className={styles.button}>Submit</button>
-        </form>
+                    autoSize={{ minRows: 3, maxRows: 6 }}
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                    Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
     );
 }
+    
 
 export default ContactForm;
